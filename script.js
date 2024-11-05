828a61f3823b5dd75a73acfd3a3bf39ab74907fb
Duygu Dağdelen <101258339+dduyg@users.noreply.github.com>
2024-11-05 21:02:56 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// ---------------------------------------- \\
+ //      Anchor Hover Effects for Body Copy    \\
+// -------------------------------------------- \\
+
+// 1. Pseudoscience uses the ::after element with a transition set on the position and width of the pseudo element.
+
+// 2. Chupacabras uses both the ::before and ::after pseudo elements. One to form the underline, and the other to transform into a magical button while the <a> tag itself scales on hover.
+
+// 3. Zoology uses an ::after element with a transition set on the height, background colour, and position.
+
+// 4. Folkloristics uses both the ::before and ::after pseudo elements. The first to form the underline and the second positioned on the left, while transitioning the width on :hover.
